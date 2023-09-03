544d3d7423c07bb0c8d91a4efda55e1ae0072e7c
Carlos Eduardo <carlospintorossy07@gmail.com>
2023-09-02 20:42:36 -0400

    teste funcoes de soma

--- test_soma.py
+++ test_soma.py
@@ -1,4 +1,13 @@
 from soma import funcao_soma
 
-def test_soma():
+def test_soma_positivos():
     assert funcao_soma(2, 3) == 5
+
+def test_soma_negativos():
+    assert funcao_soma(-2, -3) == -5
+
+def test_soma_misto():
+    assert funcao_soma(2, -3) == -1
+
+def test_soma_incorreta():
+    assert funcao_soma(2, 3) == 10
