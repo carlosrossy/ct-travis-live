d50d763a0e60f444e0787f993cce313d98af0280
Carlos Eduardo <carlospintorossy07@gmail.com>
2023-09-02 20:59:16 -0400

    teste funcoes

--- test_soma.py
+++ test_soma.py
@@ -9,5 +9,5 @@ def test_soma_negativos():
 def test_soma_misto():
     assert funcao_soma(2, -3) == -1
 
-def test_soma_incorreta():
-    assert funcao_soma(2, 3) == 10
+# def test_soma_incorreta():
+#     assert funcao_soma(2, 3) == 10
