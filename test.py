42ae81c566269a77aaff78984241857896b5014f
Carlos Eduardo <carlospintorossy07@gmail.com>
2023-09-02 20:34:11 -0400

    travis

--- test.py
+++
@@ -1,5 +0,0 @@
-def test_soma():
-    assert soma(2, 3) == 5
-
-def soma(a, b):
-    return a + b
---
+++ test_soma.py
@@ -0,0 +1,4 @@
+from soma import funcao_soma
+
+def test_soma():
+    assert funcao_soma(2, 3) == 5
